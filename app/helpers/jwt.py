6de9ba21667634786dd4750b0543f28167854c94
RupeshLineseDev <rupesh@linese.co>
2025-12-04 14:54:31 +0530

    create jwt handler file

---
+++ app/helpers/jwt.py
@@ -0,0 +1,18 @@
+from PyJWT import jwt
+from datetime import datetime, timedelta
+from config import JWT_SECRET_KEY, JWT_ALGORITHM, JWT_TOKEN_TIME_HOURS
+
+def create_jwt_token(data: dict) -> str:
+    to_encode = data.copy()
+    to_encode['exp'] = datetime.utcnow() + timedelta(hours=JWT_TOKEN_TIME_HOURS)
+    encoded_jwt = jwt.encode(to_encode, JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
+    return encoded_jwt
+
+def decode_jwt_token(token: str) -> dict:
+    try:
+        decoded_jwt = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])
+        return decoded_jwt
+    except jwt.ExpiredSignatureError:
+        raise Exception("Token has expired")
+    except jwt.InvalidTokenError:
+        raise Exception("Invalid token")

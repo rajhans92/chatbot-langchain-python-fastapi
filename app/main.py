9d074b0f503f8438d62b3e3d026eba7bbab16d55
RupeshLineseDev <rupesh@linese.co>
2025-12-07 11:17:02 +0530

    set exception handler class

--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.exceptions import RequestValidationError
 from app.helpers.database import engine, Base
 from app.routers import users
+from app.helpers.exceptionHandler import (
+    http_exception_handler,
+    validation_exception_handler,
+    value_error_handler,
+    global_exception_handler
+)
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
-app.include_router(users.router)
+
+app.add_exception_handler(Exception, global_exception_handler)
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+app.add_exception_handler(HTTPException, http_exception_handler)
+app.add_exception_handler(ValueError, value_error_handler)
+
+
+app.include_router(users.router,prefix="/api/v1")
 
 
 
